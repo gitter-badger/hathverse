ae7c020a071b73c9f0509a8b1ff9f6592d6d18c7
zhangjun <zhangjun.julian@gmail.com>
2016-04-12 11:28:21 +0800

    add maxlength in html , but lucid don't support the minlength tag

--- Hathverse/View/Signup.hs
+++ Hathverse/View/Signup.hs
@@ -20,16 +20,16 @@ signupView = withTitleBody "signup" $ do
         table_ [class_ "table table-bordered table-hover"] $ do
             tr_ $ do
                 td_ "fullname"
-                td_ $ input_ [type_ "text", name_ "fullname"]
+                td_ $ input_ [type_ "text", name_ "fullname",maxlength_ "20"]
             tr_ $ do
-                td_ "username"
-                td_ $ input_ [type_ "text", name_ "username",pattern_ "[0-9a-zA-Z]+"]
-            tr_ $ do
-                td_ "password"
-                td_ $ input_ [type_ "password", name_ "password"]
+                td_ "username [0-9a-zA-Z]+"
+                td_ $ input_ [type_ "text", name_ "username",pattern_ "[0-9a-zA-Z]+",maxlength_ "20"]
             tr_ $ do
                 td_ "password"
-                td_ $ input_ [type_ "password", name_ "password2"]
+                td_ $ input_ [type_ "password", name_ "password",maxlength_ "20"]
+            tr_ $ do
+                td_ "password"
+                td_ $ input_ [type_ "password", name_ "password2",maxlength_ "20"]
             tr_ $ do
                 td_ ""
                 td_ $ input_ [type_ "submit", value_ "Submit"]
